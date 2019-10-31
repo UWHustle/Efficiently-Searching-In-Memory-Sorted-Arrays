68b49c973f0ae16fba6d86479c7fe580d7190368
Yannis Chronis <chronis@cs.wisc.edu>
2019-10-31 12:18:18 -0500

    pandas aggregation fix

--- reproduce_experiments/create_figures.sh
+++ reproduce_experiments/create_figures.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+clean.sh
+
 echo "Create all experiment configurations"
 python3 ./experiments_configurations/create_configuration.py
 
 echo "Running all experiments"
-clean.sh
 python3 ./experiments_results/run_experiments.py
 
 echo "Creating figures"
