cf116e29ea47efa1d0b1c0b904e328af277c179c
Yannis Chronis <chronis@cs.wisc.edu>
2019-11-01 08:49:32 -0500

    nits

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 sudo apt-get update
-yes | sudo apt install -y libomp-dev python3 python3-pandas g++
+sudo apt install -y libomp-dev python3 python3-pandas g++
--- reproduce_experiments/install.sh
+++ reproduce_experiments/install.sh
@@ -1,3 +1,3 @@
 cd .. && ./install.sh && cd reproduce_experiments
 make -C ..
-yes | sudo apt-get install -y python3-pip python3-dev jupyter
+sudo apt-get install -y python3-pip python3-dev jupyter
