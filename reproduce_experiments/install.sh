b132ea17a0de8e3d2193d9541358aa69a47b5bdf
Yannis Chronis <chronis@cs.wisc.edu>
2019-11-19 13:10:19 -0600

    install necessary python packages

--- reproduce_experiments/install.sh
+++ reproduce_experiments/install.sh
@@ -3,6 +3,10 @@ make searchbench -C ..
 
 if [[ `uname` == "Darwin" ]]; then
         pip3 install jupyter
+        pip3 install matplotlib
+        pip3 install numpy
 else
         sudo apt-get install -y jupyter
+        pip3 install matplotlib
+        pip3 install numpy
 fi
